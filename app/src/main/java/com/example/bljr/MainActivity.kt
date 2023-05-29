5804f81ba1104d55f0910da9f22960352725db9d
Redmibook <nisaapriliani016@gmail.com>
2023-05-29 20:23:32 +0700

    upload

---
+++ app/src/main/java/com/example/bljr/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.bljr
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var login = findViewById<Button>(R.id.btn_login)
+        login.setOnClickListener {
+            val pindah = Intent(this, HalamanLogin::class.java);
+            startActivity(pindah)
+        }
+    }
+}
